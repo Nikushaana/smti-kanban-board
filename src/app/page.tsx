d21548ed4e672dc4afc40ffe3530b0a774620b43
Nikushaana <nikanashvili@gmail.com>
2026-01-11 21:52:24 +0400

    feat: created skeleton loader, rewrite all cards, columns in components, installed react query for fetching data, created backend api to fetch and filter data with client name, date and value, also filtered data is controlled from url params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import { Suspense } from "react";
 import KanbanBoard from "./components/kanban/kanbanBoard";
 import KanbanFilter from "./components/kanban/kanbanFilter";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen h-full flex flex-col gap-y-5 pt-10 pb-7 px-5 lg:px-10 ">
-      <KanbanFilter />
-      <KanbanBoard />
-    </div>
+    <Suspense fallback={<p>Loading...</p>}>
+      <div className="w-full min-h-screen h-full flex flex-col gap-y-5 pt-10 pb-7 px-5 lg:px-10 ">
+        <KanbanFilter />
+        <KanbanBoard />
+      </div>
+    </Suspense>
   );
 }
